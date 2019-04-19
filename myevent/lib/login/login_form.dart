fcf7e1c704e5211a06d221cb58564e3617545cde
Junior Contreras <junior.contreras@mvi-solve-it.com>
2019-04-20 00:40:46 +0200

    [JCQ] initial login page added

---
+++ myevent/lib/login/login_form.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:HMCommunity/authentication/authentication.dart';
+import 'package:HMCommunity/login/login.dart';
+
+class LoginForm extends StatefulWidget {
+  final LoginBloc loginBloc;
+  final AuthenticationBloc authenticationBloc;
+
+  LoginForm({
+    Key key,
+    @required this.loginBloc,
+    @required this.authenticationBloc,
+  }) : super(key: key);
+
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  LoginBloc get _loginBloc => widget.loginBloc;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<LoginEvent, LoginState>(
+      bloc: _loginBloc,
+      builder: (
+          BuildContext context,
+          LoginState state,
+          ) {
+        if (state is LoginFailure) {
+          _onWidgetDidBuild(() {
+            Scaffold.of(context).showSnackBar(
+              SnackBar(
+                content: Text('${state.error}'),
+                backgroundColor: Colors.red,
+              ),
+            );
+          });
+        }
+
+        return Form(
+          child: Column(
+            children: [
+              TextFormField(
+                //decoration: InputDecoration(labelText: 'username'),
+                decoration: InputDecoration(labelText: 'username', contentPadding: EdgeInsets.only(left: 15.0)),
+                controller: _usernameController,
+                textAlign: TextAlign.start,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'password'),
+                controller: _passwordController,
+                obscureText: true,
+              ),
+              RaisedButton(
+                onPressed:
+                state is! LoginLoading ? _onLoginButtonPressed : null,
+                child: Text('Login'),
+              ),
+              Container(
+                child:
+                state is LoginLoading ? CircularProgressIndicator() : null,
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  void _onWidgetDidBuild(Function callback) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      callback();
+    });
+  }
+
+  _onLoginButtonPressed() {
+    _loginBloc.dispatch(LoginButtonPressed(
+      username: _usernameController.text,
+      password: _passwordController.text,
+    ));
+  }
+}
