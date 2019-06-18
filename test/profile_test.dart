eeadd51350b4add17f9a59d87a44f32a81468018
tamar <isaeva@hm.edu>
2019-06-18 16:29:55 +0200

    profile_test erweitert; commit für coverage check

--- test/profile_test.dart
+++ test/profile_test.dart
@@ -1,23 +1,70 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_campus_connected/models/profile_item.dart';
+import 'package:flutter_campus_connected/models/user_entity.dart';
 import 'package:flutter_campus_connected/pages/profile.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'test_helper.dart';
 
+
 void main() {
-  testWidgets('profile test', (WidgetTester tester) async {
-    var profilePage = ProfilePage(
-      firebaseUser: null,
-    );
-    var curr = TestHelper.buildPage(profilePage);
-    await tester.pumpWidget(curr);
+  group('profile test', () {
 
-    final editText = find.text('Edit');
-    expect(editText, findsOneWidget);
+    test('profileItem test', () {
+      final entity =  ProfileItem('test', 'test', 'test.png' );
+      expect(entity, isNotNull);
+      expect(entity.eventDescription, isNotEmpty);
+      expect(entity.eventName, isNotEmpty);
+      expect(entity.eventPhotoUrl, isNotEmpty);
 
-    final myEvents = find.text('My Events');
-    expect(myEvents, findsOneWidget);
+      expect(entity.eventName, equals('test'));
+      expect(entity.eventPhotoUrl, equals('test.png'));
+      expect(entity.eventDescription, 'test');
 
-    final testCircle = find.byType(Text);
-    expect(testCircle, findsWidgets);
+    });
+
+    testWidgets('profile widget test', (WidgetTester tester) async {
+      var profilePage = ProfilePage(firebaseUser: null);
+      var curr = TestHelper.buildPage(profilePage) ;
+      await tester.pumpWidget(curr);
+
+      final editText = find.text('Edit');
+      expect(editText, findsOneWidget);
+
+      final myEvents = find.text('My Events');
+      expect(myEvents, findsOneWidget);
+
+      final testCircle = find.byType(Text);
+      expect(testCircle, findsWidgets);
+
+      final BuildContext context = tester.element(editText);
+
+      //state
+      final ProfilePageState state = tester.state(find.byType(ProfilePage));
+      expect(state, isNotNull);
+      expect(state.widget, equals(profilePage));
+
+      final btEdit =state.getBtEdit(context);
+      TestHelper.checkWidget<FlatButton>(btEdit);
+
+      final body =state.getBody(context);
+      TestHelper.checkWidget<Column>(body);
+
+      final appBar = state.appBar(context);
+      TestHelper.checkWidget<AppBar>(appBar);
+
+      final userJ = state.userEvents();
+      TestHelper.checkWidget<Expanded>(userJ);
+
+      final entity =  ProfileItem('test', 'test', 'test.png' );
+      var item = state.getItem(entity, 1, context, '1', null);
+
+      TestHelper.checkWidget<Card>(item);
+
+      final userEntity =  UserEntity('test', 'test.png', 'test@test.com', null);
+      var userItem = state.getProfileItem(userEntity,  context);
+      TestHelper.checkWidget<Column>(userItem);
+
+    });
   });
 }
